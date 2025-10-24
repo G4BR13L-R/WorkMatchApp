3e32387e5b3505a4e9ab14efe1e1d32e3a0bb9df
Gabriel Silva de Rezende <gabrielrezende23@hotmail.com>
2025-10-24 00:41:58 -0400

    Ajustado metodos do controller de candidaturas do contratante

--- lib/core/controllers/contratante/candidatura_controller.dart
+++ lib/core/controllers/contratante/candidatura_controller.dart
@@ -4,24 +4,12 @@ import 'package:work_match_app/core/services/contratante/candidatura_service.dar
 class CandidaturaController {
   final CandidaturaService _candidaturaService = CandidaturaService();
 
-  Future<List<CandidaturaModel>> index(int id) {
-    return _candidaturaService.index(id);
+  Future<List<CandidaturaModel>> index(int ofertaId) {
+    return _candidaturaService.index(ofertaId);
   }
 
   Future<CandidaturaModel> show(int id) {
     return _candidaturaService.show(id);
-  }
-
-  Future<CandidaturaModel> register(int ofertaId, double salario) {
-    return _candidaturaService.store(ofertaId, salario);
-  }
-
-  Future<bool> update(int id, int ofertaID, double salario) {
-    return _candidaturaService.update(id, ofertaID, salario);
-  }
-
-  Future<bool> destroy(int id) {
-    return _candidaturaService.destroy(id);
   }
 
   Future<bool> changeStatus(int candidaturaId, int status) {
