b098bc3add06b6c776fe83763e7746445624a986
Gabriel Silva de Rezende <gabrielrezende23@hotmail.com>
2025-10-24 00:42:39 -0400

    Ajustado metodos do service de candidatura do contratante

--- lib/core/services/contratante/candidatura_service.dart
+++ lib/core/services/contratante/candidatura_service.dart
@@ -24,37 +24,8 @@ class CandidaturaService {
     return CandidaturaModel.fromJson(data);
   }
 
-  Future<CandidaturaModel> store(int ofertaId, double salario) async {
-    final response = await ApiClient.post('/contratado/candidaturas', {
-      'oferta_id': ofertaId,
-      'salario': salario,
-      'status_id': 1, // 1 - Inscrito
-    });
-
-    if (response.statusCode != 201) ThrowException.request(response.body);
-
-    final data = jsonDecode(response.body);
-    return CandidaturaModel.fromJson(data);
-  }
-
-  Future<bool> update(int id, int ofertaId, double salario) async {
-    final response = await ApiClient.put('/contratante/candidaturas/$id', {'oferta_id': ofertaId, 'salario': salario});
-
-    if (response.statusCode != 200) return ThrowException.request(response.body);
-
-    return true;
-  }
-
-  Future<bool> destroy(int id) async {
-    final response = await ApiClient.delete('/contratante/candidaturas/$id');
-
-    if (response.statusCode != 200) return ThrowException.request(response.body);
-
-    return true;
-  }
-
-  Future<bool> changeStatus(int candidaturaId, int status) async {
-    final response = await ApiClient.put('/contratante/candidaturas/$candidaturaId', {'status_id': status});
+  Future<bool> changeStatus(int id, int status) async {
+    final response = await ApiClient.put('/contratante/candidaturas/$id', {'status_id': status});
 
     if (response.statusCode != 200) return ThrowException.request(response.body);
 
