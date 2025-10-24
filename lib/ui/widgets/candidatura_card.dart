f4a4efbc0e3906450fb6702885834cade2bc9da9
Gabriel Silva de Rezende <gabrielrezende23@hotmail.com>
2025-10-24 00:42:15 -0400

    Melhorado card de candidatura

--- lib/ui/widgets/candidatura_card.dart
+++ lib/ui/widgets/candidatura_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:work_match_app/core/models/oferta_model.dart';
+import 'package:work_match_app/core/models/status_model.dart';
 import 'package:work_match_app/core/theme/app_colors.dart';
 import 'package:work_match_app/core/theme/app_text_styles.dart';
 import 'package:work_match_app/ui/widgets/custom_button.dart';
@@ -8,9 +10,11 @@ class CandidaturaCard extends StatelessWidget {
   final double salario;
   final String cidade;
   final String estado;
-  final String status;
-  final VoidCallback onContratar;
-  final VoidCallback onReprovar;
+  final StatusModel status;
+  final OfertaModel oferta;
+  final VoidCallback? onContratar;
+  final VoidCallback? onReprovar;
+  final VoidCallback? onAvaliar;
 
   const CandidaturaCard({
     super.key,
@@ -19,8 +23,10 @@ class CandidaturaCard extends StatelessWidget {
     required this.cidade,
     required this.estado,
     required this.status,
-    required this.onContratar,
-    required this.onReprovar,
+    required this.oferta,
+    this.onContratar,
+    this.onReprovar,
+    this.onAvaliar,
   });
 
   @override
@@ -29,7 +35,7 @@ class CandidaturaCard extends StatelessWidget {
       decoration: BoxDecoration(
         color: AppColors.inputBackground,
         borderRadius: BorderRadius.circular(12),
-        border: Border.all(color: AppColors.primary),
+        border: Border.all(color: status.id == 1 ? AppColors.primary : AppColors.accent),
       ),
       padding: const EdgeInsets.all(16),
       child: Column(
@@ -38,32 +44,46 @@ class CandidaturaCard extends StatelessWidget {
           Text(nome, style: AppTextStyles.titleOferta),
           const SizedBox(height: 4),
 
-          Text("$cidade - $estado", style: AppTextStyles.subtitleOferta),
-          Text("Pretensão: R\$ $salario", style: AppTextStyles.subtitleOferta),
-          Text("Status: $status", style: AppTextStyles.subtitleOferta),
+          Text("Status: ${status.descricao}", style: AppTextStyles.subtitleOferta),
+          Text("Pretensão: R\$ ${salario.toStringAsFixed(2)}", style: AppTextStyles.subtitleOferta),
+          Text("Localização: $cidade - $estado", style: AppTextStyles.subtitleOferta),
           const SizedBox(height: 16),
 
           Row(
             children: [
-              Expanded(
-                child: CustomButton(
-                  text: 'Contratar',
-                  backgroundColor: AppColors.primary,
-                  textStyle: AppTextStyles.buttonOferta,
-                  onPressed: onContratar,
+              if (status.id == 1) ...[
+                Expanded(
+                  child: CustomButton(
+                    text: 'Contratar',
+                    backgroundColor: AppColors.primary,
+                    textStyle: AppTextStyles.buttonOferta,
+                    onPressed: onContratar,
+                  ),
                 ),
-              ),
+                const SizedBox(width: 8),
+              ],
 
-              const SizedBox(width: 8),
-              
-              Expanded(
-                child: CustomButton(
-                  text: 'Reprovar',
-                  backgroundColor: AppColors.warning,
-                  textStyle: AppTextStyles.buttonOferta,
-                  onPressed: onReprovar,
+              if (!oferta.finalizada) ...[
+                Expanded(
+                  child: CustomButton(
+                    text: 'Reprovar',
+                    backgroundColor: AppColors.warning,
+                    textStyle: AppTextStyles.buttonOferta,
+                    onPressed: onReprovar,
+                  ),
                 ),
-              ),
+              ],
+
+              if (oferta.finalizada) ...[
+                Expanded(
+                  child: CustomButton(
+                    text: 'Avaliar',
+                    backgroundColor: AppColors.primary,
+                    textStyle: AppTextStyles.buttonOferta,
+                    onPressed: onAvaliar,
+                  ),
+                ),
+              ],
             ],
           ),
         ],
