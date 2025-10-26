2096ec34503ba7dd744c1c0258cfe9cf3eea59c6
Gabriel Silva de Rezende <gabrielrezende23@hotmail.com>
2025-10-26 11:38:50 -0400

    Implementado botão de contato via whatsapp e ajustado condições dos botões

--- lib/ui/widgets/candidatura_card.dart
+++ lib/ui/widgets/candidatura_card.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:work_match_app/core/models/oferta_model.dart';
 import 'package:work_match_app/core/models/status_model.dart';
 import 'package:work_match_app/core/theme/app_colors.dart';
 import 'package:work_match_app/core/theme/app_text_styles.dart';
+import 'package:work_match_app/core/utils/snackbar_helper.dart';
 import 'package:work_match_app/ui/widgets/custom_button.dart';
 
 class CandidaturaCard extends StatelessWidget {
   final String nome;
+  final String telefone;
   final double salario;
   final String cidade;
   final String estado;
@@ -19,6 +22,7 @@ class CandidaturaCard extends StatelessWidget {
   const CandidaturaCard({
     super.key,
     required this.nome,
+    required this.telefone,
     required this.salario,
     required this.cidade,
     required this.estado,
@@ -31,11 +35,13 @@ class CandidaturaCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final styleCard = {1: AppColors.primary, 2: AppColors.accent, 3: AppColors.warning};
+
     return Container(
       decoration: BoxDecoration(
         color: AppColors.inputBackground,
         borderRadius: BorderRadius.circular(12),
-        border: Border.all(color: status.id == 1 ? AppColors.primary : AppColors.accent),
+        border: Border.all(color: styleCard[status.id] ?? AppColors.primary),
       ),
       padding: const EdgeInsets.all(16),
       child: Column(
@@ -51,30 +57,46 @@ class CandidaturaCard extends StatelessWidget {
 
           Row(
             children: [
-              if (status.id == 1) ...[
-                Expanded(
-                  child: CustomButton(
-                    text: 'Contratar',
-                    backgroundColor: AppColors.primary,
-                    textStyle: AppTextStyles.buttonOferta,
-                    onPressed: onContratar,
-                  ),
-                ),
-                const SizedBox(width: 8),
-              ],
-
               if (!oferta.finalizada) ...[
-                Expanded(
-                  child: CustomButton(
-                    text: 'Reprovar',
-                    backgroundColor: AppColors.warning,
-                    textStyle: AppTextStyles.buttonOferta,
-                    onPressed: onReprovar,
+                if (status.id != 2) ...[
+                  Expanded(
+                    child: CustomButton(
+                      text: 'Contratar',
+                      backgroundColor: AppColors.primary,
+                      textStyle: AppTextStyles.buttonOferta,
+                      onPressed: onContratar,
+                    ),
                   ),
-                ),
+                ],
+
+                if (status.id == 1) ...[const SizedBox(width: 8)],
+
+                if (status.id != 3) ...[
+                  Expanded(
+                    child: CustomButton(
+                      text: 'Reprovar',
+                      backgroundColor: AppColors.warning,
+                      textStyle: AppTextStyles.buttonOferta,
+                      onPressed: onReprovar,
+                    ),
+                  ),
+                ],
               ],
 
               if (oferta.finalizada) ...[
+                Expanded(
+                  child: CustomButton(
+                    text: 'WhatsApp',
+                    backgroundColor: AppColors.success,
+                    textStyle: AppTextStyles.buttonOferta,
+                    onPressed:
+                        () => openWhatsapp(
+                          telefone,
+                          onError: () => SnackbarHelper.showError(context, 'Erro ao abrir o WhatsApp.'),
+                        ),
+                  ),
+                ),
+
                 Expanded(
                   child: CustomButton(
                     text: 'Avaliar',
@@ -89,5 +111,17 @@ class CandidaturaCard extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  Future<void> openWhatsapp(String telefone, {required VoidCallback onError}) async {
+    final sanitized = telefone.replaceAll(RegExp(r'\D'), '');
+    final Uri url = Uri.parse('https://wa.me/55$sanitized');
+
+    try {
+      final success = await launchUrl(url, mode: LaunchMode.platformDefault);
+      if (!success) onError();
+    } catch (e) {
+      onError();
+    }
   }
 }
